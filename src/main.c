8e6dc8c00daa177fb69b39124903ece1cfd8827c
Alex Marincu <alex.marincu@gmail.com>
2020-07-06 19:45:50 +0300

    Added return statement to main function

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "Application/Application.h"
 #include <stdio.h>
 
-Void main()
+Int8 main()
 {
     mApplication Pt application = Application_getInstance();
     Application_init(application, null);
     printf("application.objectSize() = %d\n", CObject_objectSize((CObject Pt) application));
     Application_start(application);
+    return 0;
 }
