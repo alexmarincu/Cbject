376e8c3e7274c66e406537e89ad71472422b6ec8
Alex Marincu <alex.marincu@gmail.com>
2021-10-19 22:08:29 +0300

    Added some tests for Rectangle object

--- test/test_Rectangle.c
+++ test/test_Rectangle.c
@@ -1,16 +1,62 @@
-#include "Cbject.h"
+#include "unity.h"
+
 #include "Rectangle.h"
 #include "Shape.h"
-#include "unity.h"
+
+TEST_FILE("Cbject.c")
+TEST_FILE("Rectangle.c")
+TEST_FILE("Shape.c")
+
+static void checkRectangleInit(Rectangle const * const r);
+static void checkRectanglePropsAccessors(Rectangle const * const r);
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_Rectanglesss(void)
+/*============================================================================= tag::testDoc[]
+== Rectangle on heap
+.Description
+Test creation on heap and props accessors of Rectangle object
+
+=============================================================================== end::testDoc[] */
+void test_Rectangle_Heap(void)
 {
     Rectangle * r = New(Rectangle, &((RectangleParams){.origin.x = 4, .origin.y = 5, .width = 1, .height = 2}));
-    TEST_ASSERT_NOT_NULL(r);
-    TEST_ASSERT_EQUAL_UINT32(4, Shape_origin(Rectangle_toShape(r)).x);
+    checkRectangleInit(r);
+    checkRectanglePropsAccessors(r);
     free(r);
+}
+
+/*============================================================================= tag::testDoc[]
+== Rectangle on static pool
+.Description
+Test retrieval from static pool and props accessors of Rectangle object
+
+=============================================================================== end::testDoc[] */
+void test_Rectangle_StaticPool(void)
+{
+    Rectangle * r = Get(Rectangle, &((RectangleParams){.origin.x = 4, .origin.y = 5, .width = 1, .height = 2}));
+    checkRectangleInit(r);
+    checkRectanglePropsAccessors(r);
+}
+
+static void checkRectangleInit(Rectangle const * const r)
+{
+    TEST_ASSERT_NOT_NULL(r);
+    TEST_ASSERT_EQUAL_UINT8(4, Shape_origin(Rectangle_toShape(r)).x);
+    TEST_ASSERT_EQUAL_UINT8(5, Shape_origin(Rectangle_toShape(r)).y);
+    TEST_ASSERT_EQUAL_UINT8(1, Rectangle_width(r));
+    TEST_ASSERT_EQUAL_UINT8(2, Rectangle_height(r));
+}
+
+static void checkRectanglePropsAccessors(Rectangle const * const r)
+{
+    Shape_originSet(r, (Point){1, 2});
+    TEST_ASSERT_EQUAL_UINT8(1, Shape_origin(Rectangle_toShape(r)).x);
+    TEST_ASSERT_EQUAL_UINT8(2, Shape_origin(Rectangle_toShape(r)).y);
+    Rectangle_widthSet(r, 2);
+    Rectangle_heightSet(r, 3);
+    TEST_ASSERT_EQUAL_UINT8(2, Rectangle_width(r));
+    TEST_ASSERT_EQUAL_UINT8(3, Rectangle_height(r));
 }
