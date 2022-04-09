b63f382fcd982eb74bfffac3db55c76e403a7231
Alex Marincu <alex.marincu@gmail.com>
2022-04-09 18:08:15 +0300

    .

--- test/test_Rectangle.c
+++ test/test_Rectangle.c
@@ -25,12 +25,35 @@ void tearDown(void) {
 /**
  * @brief
  */
-void test_Rectangle_Heap(void) {
-    Rectangle * r1 = Rectangle_init(new_(Rectangle), (Point){ 0, 0 }, 5, 7);
-    Rectangle * r2 = copy_(Rectangle, r1);
-    TEST_ASSERT_EQUAL_UINT8(5 * 7, Shape_area(Rectangle_getShape(r1)));
-    TEST_ASSERT_EQUAL_UINT8(5 * 7, Shape_area(Rectangle_getShape(r2)));
-    Drawable_draw(Rectangle_getDrawable(r1));
-    delete_(r1);
-    delete_(r2);
+void test_Rectangle_init(void) {
+    Rectangle * r = Rectangle_init(new_(Rectangle), (Point){ 0, 1 }, 2, 3);
+    TEST_ASSERT_EQUAL_UINT32(2, Rectangle_getWidth(r));
+    TEST_ASSERT_EQUAL_UINT32(3, Rectangle_getHeight(r));
+    TEST_ASSERT_EQUAL_UINT8(2 * 3, Shape_area(Rectangle_getShape(r)));
+    delete_(r);
+}
+
+/**
+ * @brief
+ */
+void test_Rectangle_setters(void) {
+    Rectangle * r = Rectangle_init(new_(Rectangle), (Point){ 0, 1 }, 2, 3);
+    Rectangle_setWidth(r, 4);
+    Rectangle_setHeight(r, 5);
+    TEST_ASSERT_EQUAL_UINT32(4, Rectangle_getWidth(r));
+    TEST_ASSERT_EQUAL_UINT32(5, Rectangle_getHeight(r));
+    TEST_ASSERT_EQUAL_UINT8(4 * 5, Shape_area(Rectangle_getShape(r)));
+    delete_(r);
+}
+
+/**
+ * @brief
+ */
+void test_Rectangle_makeSquare(void) {
+    Rectangle * r = Rectangle_init(new_(Rectangle), (Point){ 0, 1 }, 2, 3);
+    Rectangle_makeSquare(r, 4);
+    TEST_ASSERT_EQUAL_UINT32(4, Rectangle_getWidth(r));
+    TEST_ASSERT_EQUAL_UINT32(4, Rectangle_getHeight(r));
+    TEST_ASSERT_EQUAL_UINT8(4 * 4, Shape_area(Rectangle_getShape(r)));
+    delete_(r);
 }
